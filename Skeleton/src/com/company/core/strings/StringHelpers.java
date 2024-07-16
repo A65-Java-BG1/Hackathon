5794ccd4d09efbddd97daf61a2a3deb649b28d48
mihailboychev <mikweew@gmail.com>
2024-07-16 17:30:36 +0300

    Implement Concat, firstIndexOf, padEnd, repeat, section Methods in StringHelpers

--- Skeleton/src/com/company/core/strings/StringHelpers.java
+++ Skeleton/src/com/company/core/strings/StringHelpers.java
@@ -34,7 +34,23 @@ public class StringHelpers {
         return null;
     }
 
+    /**
+     * Checks if source contains a symbol.
+     *
+     * @param source The string to check
+     * @param symbol The character to check for
+     * @return boolean - true if symbol is within source or false if not
+     *
+     * @author Mihail Boychev
+     */
     public static boolean contains(String source, char symbol) {
+
+        for (int i = 0; i < source.length(); i++) {
+            if (source.charAt(i) == symbol) {
+                return true;
+            }
+        }
+
         return false;
     }
 
@@ -42,8 +58,24 @@ public class StringHelpers {
         return false;
     }
 
+    /**
+     * Finds the first index of target within source.
+     *
+     * @param source The string to check
+     * @param target The character to check for
+     * @return int - The first index of target within source, otherwise, -1
+     *
+     * @author Mihail Boychev
+     */
     public static int firstIndexOf(String source, char target) {
-        return 0;
+
+        for (int i = 0; i < source.length(); i++) {
+            if (source.charAt(i) == target) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     public static int lastIndexOf(String source, char symbol) {
@@ -55,8 +87,24 @@ public class StringHelpers {
 
     }
 
+    /**
+     * Pads source on the right side with paddingSymbol enough times to reach length.
+     *
+     * @param source The string to pad
+     * @param length The length of the string to achieve
+     * @param paddingSymbol The character used as padding
+     * @return String - The padded string
+     *
+     * @author Mihail Boychev
+     */
     public static String padEnd(String source, int length, char paddingSymbol) {
-        return null;
+        int timesToPad = length - source.length();
+
+        for (int i = 0; i < timesToPad; i++) {
+            source += paddingSymbol;
+        }
+
+        return source;
     }
 
     public static String padStart(String source, int length, char paddingSymbol) {
@@ -64,16 +112,49 @@ public class StringHelpers {
 
     }
 
+    /**
+     * Repeats the given string times.
+     *
+     * @param source The string to repeat
+     * @param times The number of times to repeat the string
+     * @return The repeated string
+     *
+     * @author Mihail Boychev
+     */
     public static String repeat(String source, int times) {
-        return null;
+
+        String newString = source;
+
+        for (int i = 1; i < times; i++) {
+            newString += source;
+        }
+
+        return newString;
     }
 
     public static String reverse(String source) {
         return null;
     }
 
+    /**
+     * Returns a new string, starting from start and ending at end.
+     *
+     * @param source The string to reverse
+     * @param start The starting position in source (inclusive)
+     * @param end The end position in source (inclusive)
+     * @return String - A new string, formed by the characters in source, starting from start to end
+     *
+     * @author Mihail Boychev
+     */
     public static String section(String source, int start, int end) {
-        return null;
+
+        String newString = "";
+
+        for (int i = start; i <= end; i++) {
+            newString += source.charAt(i);
+        }
+
+        return newString;
     }
 
     public static boolean startsWith(String source, char target) {
