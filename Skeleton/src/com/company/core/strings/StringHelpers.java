dab9f58822a175d341fba03a1044b9c9694b5d0f
SimonAnkov <simonankov1@gmail.com>
2024-07-16 17:35:01 +0300

    Filled StringHelpers methods (abbreviate, endsWith, pad, startsWith)

--- Skeleton/src/com/company/core/strings/StringHelpers.java
+++ Skeleton/src/com/company/core/strings/StringHelpers.java
@@ -6,24 +6,26 @@ public class StringHelpers {
     /**
      * Abbreviates a string using ellipses.
      *
-     * @param source The string to modify
+     * @param source    The string to modify
      * @param maxLength Maximum length of the resulting string
      * @return The abbreviated String.
-     *
      * @author Simon Ankov
      */
     public static String abbreviate(String source, int maxLength) {
-        if (maxLength >= source.length()){
+        if (maxLength >= source.length()) {
             return source;
         }
 
         String abbreviation = "";
 
         for (int i = 0; i < maxLength; i++) {
-
+            abbreviation += source.charAt(i);
         }
 
-        return null;
+        for (int i = abbreviation.length() - 1; i <= source.length(); i++) {
+            abbreviation += ".";
+        }
+        return abbreviation;
     }
 
     public static String capitalize(String source) {
@@ -38,7 +40,22 @@ public class StringHelpers {
         return false;
     }
 
+    /**
+     * Checks if the string source ends with the given character.
+     *
+     * @param source The string to check
+     * @param target The character to check for
+     * @return true if the string ends with target, else false
+     * @author Simon Ankov
+     */
     public static boolean endsWith(String source, char target) {
+        if (!source.isEmpty() && source.charAt(source.length() - 1) == target) {
+            return true;
+        }
+
+        if (source.isEmpty() && target == Character.MIN_VALUE) {
+            return true;
+        }
         return false;
     }
 
@@ -50,9 +67,26 @@ public class StringHelpers {
         return 0;
     }
 
+    /**
+     * Pads string on the left and right sides if it's shorter than length.
+     *
+     * @param source        The string to pad
+     * @param length        The length of the string to achieve
+     * @param paddingSymbol The character used as padding
+     * @return The padded string
+     * @author Simon Ankov
+     */
     public static String pad(String source, int length, char paddingSymbol) {
-        return null;
+        if (length <= source.length()) {
+            return source;
+        }
 
+        int paddings = (length - source.length()) / 2;
+        String padding = "";
+        for (int i = 0; i < paddings; i++) {
+            padding += String.valueOf(paddingSymbol);
+        }
+        return padding + source + padding;
     }
 
     public static String padEnd(String source, int length, char paddingSymbol) {
@@ -76,8 +110,21 @@ public class StringHelpers {
         return null;
     }
 
+    /**
+     * Checks if the string source starts with the given character.
+     *
+     * @param source The string to inspect
+     * @param target The character to search for
+     * @return true if string starts with target, otherwise false
+     *
+     * @author Simon Ankov
+     */
     public static boolean startsWith(String source, char target) {
-        return false;
+        if (source.isEmpty()){
+            return false;
+        }
+
+        return source.charAt(0) == target;
     }
 
 }
