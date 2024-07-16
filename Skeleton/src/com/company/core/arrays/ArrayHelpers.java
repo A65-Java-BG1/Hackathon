abb14a23709003ba06c2b1da39095ecfa2662c83
mihailboychev <mikweew@gmail.com>
2024-07-16 17:35:26 +0300

    Fix copy method in ArrayHelpers

--- Skeleton/src/com/company/core/arrays/ArrayHelpers.java
+++ Skeleton/src/com/company/core/arrays/ArrayHelpers.java
@@ -97,7 +97,7 @@ public class ArrayHelpers {
      */
     public static void copy(int[] sourceArray, int[] destinationArray, int count) {
         for (int i = 0; i < count; i++) {
-            if (count >= sourceArray.length) {
+            if (i >= sourceArray.length) {
                 destinationArray[i] = 0;
             }
             else {
