97c4de2e5fe0a1ebdb630f50a906033f666c2f04
Edis <chopito11976@gmail.com>
2024-07-16 17:27:54 +0300

    capitalize, concat, lastIndexOf, padStart, reverse

--- Skeleton/src/com/company/core/strings/StringHelpers.java
+++ Skeleton/src/com/company/core/strings/StringHelpers.java
@@ -7,12 +7,43 @@ public class StringHelpers {
         return null;
     }
 
+    /**
+     *Capitalizes a string changing the first character to title case. No other characters are changed. If source is empty returns empty string.
+     * @param source The string to modify
+     * @return The capitalized string or empty string if source is empty
+     *
+     * @author Edis Ismailov
+     */
     public static String capitalize(String source) {
-        return null;
+        {
+            if (source == null || source.isEmpty()) {
+                return source;
+            }
+            return Character.toUpperCase(source.charAt(0)) + source.substring(1);
+        }
+
     }
 
+    /**
+     *Concatenates string1 to the end of string2.
+     * @param string1 The left part of the new string
+     * @param string2 The right part of the new string
+     * @return A string that represents the concatenation of string1 followed by string2's characters
+     *
+     * @author Edis Ismailov
+     */
     public static String concat(String string1, String string2) {
-        return null;
+     {
+            if (string1 == null) {
+                string1 = "";
+            }
+            if (string2 == null) {
+                string2 = "";
+            }
+
+            return string1 + string2;
+        }
+
     }
 
     public static boolean contains(String source, char symbol) {
@@ -27,8 +58,21 @@ public class StringHelpers {
         return 0;
     }
 
+    /**
+     * Finds the last index of target within source.
+     * @param source String - The string to check
+     * @param symbol char - The character to check for
+     * @return The last index symbol within source or -1 if no match
+     *
+     * @author Edis Ismailov
+     */
     public static int lastIndexOf(String source, char symbol) {
-        return 0;
+        for (int i = source.length() - 1; i >= 0; i--) {
+            if (source.charAt(i) == symbol) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static String pad(String source, int length, char paddingSymbol) {
@@ -40,17 +84,49 @@ public class StringHelpers {
         return null;
     }
 
+    /**
+     * Pads source on the left side with paddingSymbol enough times to reach length length.
+     * @param source String - The string to pad
+     * @param length int - The length of the string to achieve
+     * @param paddingSymbol char - The character used as padding
+     * @return The padded string or null if source is null
+     *
+     * @author Edis Ismailov
+     */
     public static String padStart(String source, int length, char paddingSymbol) {
-        return null;
-
+        int padLength = length - source.length();
+        if (padLength <= 0) {
+            return source;
+        }
+        String format = "%" + length + "s";
+        return String.format(format, source).replace(' ', paddingSymbol);
     }
 
     public static String repeat(String source, int times) {
         return null;
     }
 
+    /**
+     *Reverses source so that the first element becomes the last, the second element becomes the second to last, and so on.
+     * @param source String - The string to reverse
+     * @return The reversed string
+     *
+     * @author Edis Ismailov
+     */
     public static String reverse(String source) {
-        return null;
+        char[] chars = source.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new String(chars);
+
     }
 
     public static String section(String source, int start, int end) {
